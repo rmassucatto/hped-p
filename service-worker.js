39b3b81cc8403d4105d3fa80e0568d9d2fa63468
rmassucatto <rmassucatto@gmail.com>
2025-08-12 23:04:20 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,45 @@
+
+const CACHE_NAME = "hped-p-v1";
+const OFFLINE_URLS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(OFFLINE_URLS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((k) => k !== CACHE_NAME && caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  // Network first for HTML; cache-first for others
+  if (req.mode === "navigate") {
+    event.respondWith(
+      fetch(req).then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE_NAME).then((c) => c.put("./", copy));
+        return res;
+      }).catch(() => caches.match("./index.html"))
+    );
+  } else {
+    event.respondWith(
+      caches.match(req).then((cached) => cached || fetch(req).then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE_NAME).then((c) => c.put(req, copy));
+        return res;
+      }))
+    );
+  }
+});
